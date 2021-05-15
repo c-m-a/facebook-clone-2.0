b580cb9b9ba1536bada145d4f91904d574cbd55e
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-05-15 18:58:12 -0500

    Add icons and Left Header section

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,44 @@
-export default Header() {
+import Image from 'next/image';
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from '@heroicons/react/solid';
+
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from '@heroicons/react/outline';
+
+export default function Header() {
   return (
-    <div></div>
+    <div>
+      {/* Left */}
+      <div className='flex items-center'>
+        <Image
+          src='https://links.papareact.com/5me'
+          width={40}
+          height={40}
+          layout='fixed'
+        />
+        <div className='flex items-center ml-2 rounded-full bg-gray-100 p-2'>
+          <SearchIcon
+            className='h-6'
+          />
+          <input
+            className='flex ml-2 items-center bg-transparent outline-none placeholder-gray-500'
+            type='text'
+            placeholder='Search Facebook'
+          />
+        </div>
+      </div>
+      {/* Middle */}
+      {/* Right */}
+    </div>
   )
 }
