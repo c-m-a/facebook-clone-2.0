42c68983d3be26a6dd299ce7d6938b28bd0ac4c9
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-05-18 00:02:51 -0500

    Add Login component in index.js and redirecting if session exits

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import { getSession } from 'next-auth/client'
 import Head from 'next/head'
-import Header from '../components/Header';
+import Header from '../components/Header'
+import Login from '../components/Login'
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />
+
   return (
     <div>
       <Head>
@@ -20,4 +24,15 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  // Get the user
+  const session = await getSession(context)
+
+  return {
+    props: {
+      session
+    }
+  }
 }
